3f102a7b2a4fdd42d0f4443183ddaf805f1357d5
dmitrykhakhin <dmitrykhakhin@gmail.com>
2021-12-07 00:37:39 +0300

    create base loop for game

---
+++ main.py
@@ -0,0 +1,21 @@
+# Pygame шаблон - скелет для нового проекта Pygame
+import pygame
+import random
+
+WIDTH = 360  # ширина игрового окна
+HEIGHT = 480  # высота игрового окна
+FPS = 30  # частота кадров в секунду
+
+# создаем игру и окно
+pygame.init()
+pygame.mixer.init()  # для звука
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+
+# Цикл игры
+running = True
+while running:
+    # Ввод процесса (события)
+    # Обновление
+    # Визуализация (сборка)
