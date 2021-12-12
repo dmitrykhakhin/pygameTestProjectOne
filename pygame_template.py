ea02e7d43cf76fe4ac5fdc78ef0c0003a25fdb22
dmitrykhakhin <dmitrykhakhin@gmail.com>
2021-12-12 21:28:34 +0300

    add sprite for player

--- pygame_template.py
+++ pygame_template.py
@@ -2,8 +2,8 @@
 import pygame
 import random
 
-WIDTH = 360  # ширина игрового окна
-HEIGHT = 480  # высота игрового окна
+WIDTH = 800  # ширина игрового окна
+HEIGHT = 650  # высота игрового окна
 FPS = 30  # частота кадров в секунду
 
 # Цвета (R, G, B)
@@ -13,6 +13,16 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+
 # создаем игру и окно
 pygame.init()
 pygame.mixer.init()  # для звука
@@ -20,6 +30,8 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 
 # Цикл игры
 running = True
